b6576e1a62d28dabe8b25bdd69f56f04d2b9ffa3
Joel9715784 <77417721+Joel9715784@users.noreply.github.com>
2021-02-03 14:02:11 -0500

    d
    dfvb

--- src/app/villanos/villanos.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, OnInit } from '@angular/core';
-@Component({
-    selector: 'app-villanos',
-    templateUrl: './villanos.component.html',
-    styleUrls: ['./villanos.component.css']
-})
-export class villanosComponent implements OnInit {
-    ngOnInit(): void { }
-}
